76659f7a1bbdadc334d110d9d11608bef264872e
Victor Sepulveda <victorseva123@gmail.com>
2023-09-29 20:46:31 -0500

    29/09/23

---
+++ Client/src/components/Home/Home.jsx
@@ -0,0 +1,13 @@
+import Cards from "../Cards/Cards";
+import styles from "./Home.module.css";
+
+const Home = (props) => {
+  const {characters, onClose} = props;
+    return (
+      <div className={styles.hom}>
+      <Cards characters={characters} onClose={onClose}/>
+      </div>
+    )
+  }
+  
+  export default Home
